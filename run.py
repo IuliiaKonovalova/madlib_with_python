479ece0e6c36154c7ce3eade93e59cc4d7c4ff71
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-24 17:00:19 +0800

    Add comments for the dictionary letters

--- run.py
+++ run.py
@@ -13,12 +13,11 @@ from colorama import Fore, Back, Style
 from simple_term_menu import TerminalMenu
 
 
-
+# Dictionary for vowels and consonants
 dictionary_letters = {
     'vowels': ['a', 'o', 'i', 'e', 'u'],
     'consonant': ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 }
-
 
 
 
