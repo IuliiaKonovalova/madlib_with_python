dd82a8ef901fc5e275cfc0e43466cdc9ce2a3d34
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-24 12:48:09 +0800

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import nltk
+import os
+
+nltk.set_proxy('127.0.0.1:41091')
+nltk.download('wordnet')
+
+from pattern.en import pluralize, conjugate, PAST
+
+import random
+
+from colorama import Fore, Back, Style
+
+from simple_term_menu import TerminalMenu
+
+
