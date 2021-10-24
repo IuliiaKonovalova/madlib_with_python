789a803470a39a570452da3904f44732f9da983e
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-24 17:20:01 +0800

    Restructure gender words file

--- gender_words.py
+++ gender_words.py
@@ -1,7 +1,8 @@
+"""
+Consist dictionary gender words
+"""
+
 dictionary_gender = {
-  """
-  Consist gender words
-  """
-  'female': ['girl', 'woman', 'girlfriend', 'daughter', 'sister', 'granddaughter', 'lady', 'nun', 'niece', 'madam', 'witch', 'baroness', 'mistress', 'landlady', 'maidservant', 'sultana', 'heroine', 'duchess', 'traitress', 'empress', 'prophetess', 'priestess', 'babygirl', 'mother', 'grandmother', 'aunt', 'wife', 'actress', 'princess', 'waitress', 'queen', 'mailwoman', 'chairwoman', 'policewoman', 'stewardess', 'congresswoman', 'saleswoman'],
-  'male': ['man', 'boy', 'boyfriend', 'son', 'brother', 'grandson' 'babyboy', 'lord', 'monk', 'nephew', 'sir', 'gentleman', 'baron', 'hunter', 'master', 'landlord', 'manservant', 'sultan', 'hero', 'duke', 'traitor', 'emperor', 'shepherd', 'abbot', 'soldier', 'prophet', 'priest', 'wizard', 'father', 'grandfather', 'uncle', 'husband', 'actor', 'prince', 'waiter', 'king', 'mailman', 'chairman', 'policeman', 'steward', 'congressman', 'salesman']
+    'female': ['girl', 'woman', 'girlfriend', 'daughter', 'sister', 'granddaughter', 'lady', 'nun', 'niece', 'madam', 'witch', 'baroness', 'mistress', 'landlady', 'maidservant', 'sultana', 'heroine', 'duchess', 'traitress', 'empress', 'prophetess', 'priestess', 'babygirl', 'mother', 'grandmother', 'aunt', 'wife', 'actress', 'princess', 'waitress', 'queen', 'mailwoman', 'chairwoman', 'policewoman', 'stewardess', 'congresswoman', 'saleswoman'],
+    'male': ['man', 'boy', 'boyfriend', 'son', 'brother', 'grandson' 'babyboy', 'lord', 'monk', 'nephew', 'sir', 'gentleman', 'baron', 'hunter', 'master', 'landlord', 'manservant', 'sultan', 'hero', 'duke', 'traitor', 'emperor', 'shepherd', 'abbot', 'soldier', 'prophet', 'priest', 'wizard', 'father', 'grandfather', 'uncle', 'husband', 'actor', 'prince', 'waiter', 'king', 'mailman', 'chairman', 'policeman', 'steward', 'congressman', 'salesman']
 }
