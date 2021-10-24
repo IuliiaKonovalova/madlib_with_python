09accb90c8203691ed2a5e0b64f70c613dbb012c
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-24 17:22:50 +0800

    Import gender dictionary in the main py file from gender_words file

--- run.py
+++ run.py
@@ -1,17 +1,13 @@
 import nltk
 import os
-
 nltk.set_proxy('127.0.0.1:41091')
 nltk.download('wordnet')
-
 from pattern.en import pluralize, conjugate, PAST
-
 import random
-
 from colorama import Fore, Back, Style
-
 from simple_term_menu import TerminalMenu
 
+from gender_words import dictionary_gender
 
 # Dictionary for vowels and consonants
 dictionary_letters = {
