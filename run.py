e3a95ffdcd9da44733f703dd39d00b8027ba4ee2
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-03 15:53:21 +0800

    Format madlib string

--- run.py
+++ run.py
@@ -71,7 +71,7 @@ def verb_formatting(verb):
     return formatted_verb
 
 
-def formatting_pronoun(noun):
+def format_pronoun(noun):
     """
     Checks the noun whether it is a female or male noun
     """
@@ -146,7 +146,7 @@ def plural_noun(noun):
     return pluralize(noun)
 
 
-def choosing_article(*words):
+def choose_article(*words):
     """
     Checks which article to use before the word and place the article before it
     """
@@ -271,207 +271,244 @@ def main_madlib():
     """
     Asks for user input, checks the input, return the madlib formatted string
     """
+    adj1 = 'easy-going'
+    noun1 = 'cat'
+    verb1 = 'show'
+    adj2 = 'furry'
+    noun2 = 'hate'
+    famous_person = 'bjorn'
+    verb2 = 'stop'
+    verb3 = 'cry'
+    noun3 = 'love'
+    size_adj1 = 'huge'
+    color_adj1 = 'red'
+    noun4 = 'cream'
+    noun5 = 'bus'
+    size_adj2 = 'tiny'
+    color_adj2 = 'blue'
+    noun6 = 'stamp'
+    noun7 = 'queen'
+    verb4 = 'sneeze'
     # Variables for a user's input
-    adj1 = ''
-    noun1 = ''
-    verb1 = ''
-    adj2 = ''
-    noun2 = ''
-    famous_person = ''
-    verb2 = ''
-    verb3 = ''
-    noun3 = ''
-    size_adjective1 = ''
-    color_adjective1 = ''
-    noun4 = ''
-    noun5 = ''
-    size_adjective2 = ''
-    color_adjective2 = ''
-    noun6 = ''
-    noun7 = ''
-    verb4 = ''
+    # adj1 = ''
+    # noun1 = ''
+    # verb1 = ''
+    # adj2 = ''
+    # noun2 = ''
+    # famous_person = ''
+    # verb2 = ''
+    # verb3 = ''
+    # noun3 = ''
+    # size_adj1 = ''
+    # color_adj1 = ''
+    # noun4 = ''
+    # noun5 = ''
+    # size_adj2 = ''
+    # color_adj2 = ''
+    # noun6 = ''
+    # noun7 = ''
+    # verb4 = ''
     # Check the the user's inputs
-    while len(adj1) < 2:
-        adj1 = input('Type an adjective: ')
-    while len(noun1) < 2:
-        noun1 = input('Type a noun (living thing): ')
-    while len(verb1) < 2:
-        verb1 = input('Type a verb: ')
-    while len(adj2) < 2:
-        adj2 = input('Type an adjective: ')
-    while len(noun2) < 2:
-        noun2 = input('Type a noun: ')
-    while len(famous_person) < 2:
-        famous_person = input("Type a famous person: ")
-    while len(verb2) < 2:
-        verb2 = input('Type a verb: ')
-    while len(verb3) < 2:
-        verb3 = input('Type a verb: ')
-    while len(noun3) < 2:
-        noun3 = input('Type a noun: ')
-    while len(size_adjective1) < 2:
-        size_adjective1 = input('Type a size adjective: ')
-    while len(color_adjective1) < 2:
-        color_adjective1 = input('Type a color adjective: ')
-    while len(noun4) < 2:
-        noun4 = input('Type a noun: ')
-    while len(noun5) < 2:
-        noun5 = input('Type a noun: ')
-    while len(size_adjective2) < 2:
-        size_adjective2 = input('Type a size adjective: ')
-    while len(color_adjective2) < 2:
-        color_adjective2 = input('Type a color adjective: ')
-    while len(noun6) < 2:
-        noun6 = input('Type a noun: ')
-    while len(noun7) < 2:
-        noun7 = input('Type a noun (living thing): ')
-    while len(verb4) < 2:
-        verb4 = input('Type a verb: ')
+    # while len(adj1) < 2:
+    #     adj1 = input('Type an adjective: ')
+    # while len(noun1) < 2:
+    #     noun1 = input('Type a noun (living thing): ')
+    # while len(verb1) < 2:
+    #     verb1 = input('Type a verb: ')
+    # while len(adj2) < 2:
+    #     adj2 = input('Type an adjective: ')
+    # while len(noun2) < 2:
+    #     noun2 = input('Type a noun: ')
+    # while len(famous_person) < 2:
+    #     famous_person = input("Type a famous person: ")
+    # while len(verb2) < 2:
+    #     verb2 = input('Type a verb: ')
+    # while len(verb3) < 2:
+    #     verb3 = input('Type a verb: ')
+    # while len(noun3) < 2:
+    #     noun3 = input('Type a noun: ')
+    # while len(size_adj1) < 2:
+    #     size_adj1 = input('Type a size adjective: ')
+    # while len(color_adj1) < 2:
+    #     color_adj1 = input('Type a color adjective: ')
+    # while len(noun4) < 2:
+    #     noun4 = input('Type a noun: ')
+    # while len(noun5) < 2:
+    #     noun5 = input('Type a noun: ')
+    # while len(size_adj2) < 2:
+    #     size_adj2 = input('Type a size adjective: ')
+    # while len(color_adj2) < 2:
+    #     color_adj2 = input('Type a color adjective: ')
+    # while len(noun6) < 2:
+    #     noun6 = input('Type a noun: ')
+    # while len(noun7) < 2:
+    #     noun7 = input('Type a noun (living thing): ')
+    # while len(verb4) < 2:
+    #     verb4 = input('Type a verb: ')
     # Madlib formatted stringbig
 
     madlib = f'''
-    {Style.BRIGHT}
-    {choosing_article(adj1).capitalize()} {noun1} {conjugate(verb1, tense = PAST)} and
-    went to {choosing_article(adj2, noun2)}.
-    {formatting_pronoun(noun1).capitalize()} noticed {famous_person.title()} {verb_infinitive(verb2)}.
-    {choosing_article(noun1).capitalize()} and {famous_person.title()} decided to {verb3} and
-    have {choosing_article(size_adjective1, color_adjective1, noun3)} together.
-    {Fore.BLUE}
-    "It will cost an arm and a leg!"{Fore.WHITE} - said the {noun1}.
-    {Fore.YELLOW}
-    "Do not worry! I have a lot of {plural_noun(noun4)}! I can afford it!"
-    {Fore.BLUE}
-    "As you wish, {famous_person.title()}. You see, I am just a poor {noun1}...
-    So I have only {plural_noun(noun5)}.
-    If you want to share {choosing_a_quantifier(noun4)} {plural_noun(noun4)},
-    let's get it!"{Fore.WHITE} - said the {noun1}.
+{Style.BRIGHT}
+{choose_article(adj1).capitalize()} {noun1} {conjugate(verb1, tense = PAST)}
+and went to {choose_article(adj2, noun2)}.
+{format_pronoun(noun1).capitalize()} noticed {famous_person.title()} {verb_infinitive(verb2)}.
+{choose_article(noun1).capitalize()} and {famous_person.title()} decided to {verb3}
+and have {choose_article(size_adj1, color_adj1, noun3)} together.
+{Fore.BLUE}
+"It will cost an arm and a leg!"{Fore.WHITE} - said the {noun1}.
+{Fore.YELLOW}
+"Do not worry! I have a lot of {plural_noun(noun4)}! I can afford it!"
+{Fore.BLUE}
+"As you wish, {famous_person.title()}. You see, I am just a poor {noun1}...
+So I have only {plural_noun(noun5)}.
+If you want to share {choosing_a_quantifier(noun4)} {plural_noun(noun4)},
+let's get it!"{Fore.WHITE} - said the {noun1}.
 
-    And they started walking towards {choosing_article(size_adjective2, color_adjective2, noun6)}.
-    It was already night when {famous_person.title()} noticed
-    {choosing_article(size_adjective2, noun6)} on the horizon.
-    {Fore.BLUE}
-    "Can you see it? We are here!"{Fore.WHITE}
-    - said {famous_person.title()} while pointing at the {noun6}.
-    {Fore.BLUE}
-    "Oh, yes! But look at this old {noun7} in the bushes!"{Fore.WHITE} - says the {noun1}.
+And they started walking towards
+{choose_article(size_adj2, color_adj2, noun6)}.
+It was already night when {famous_person.title()} noticed
+{choose_article(size_adj2, noun6)} on the horizon.
+{Fore.BLUE}
+"Can you see it? We are here!"{Fore.WHITE}
+- said {famous_person.title()} while pointing at the {noun6}.
+{Fore.BLUE}
+"Oh, yes! But look at this old {noun7} in the bushes!"
+{Fore.WHITE} - says the {noun1}.
 
-    The old {noun7} started walking slowly towards {famous_person.title()} and the {noun1}.
-    Then, they saw that {formatting_pronoun(noun7)} was carrying a golden coin.
-    As soon as {formatting_pronoun(noun7)} reached our travelers, {formatting_pronoun(noun7)}
-    started {verb_infinitive(verb4)} and handed out that golden coin to them and left silently.
-    {famous_person.title()} looked at the coin...
-    There were several words colored in {color_adjective2} and
-    a big {color_adjective2} rose drawn under them.
+The old {noun7} started walking slowly
+towards {famous_person.title()} and the {noun1}.
+Then, they saw that {format_pronoun(noun7)} was carrying a golden coin.
+As soon as {format_pronoun(noun7)} reached our travelers,
+{format_pronoun(noun7)} started {verb_infinitive(verb4)}
+and handed out that golden coin to them and left silently.
+{famous_person.title()} looked at the coin...
+There were several words colored in {color_adj2} and
+a big {color_adj2} rose drawn under them.
+{Fore.BLUE}
+"What is written there?"{Fore.WHITE} - asked the {noun1}.
+{Fore.YELLOW}
+"{idioms_list[0]}"{Fore.WHITE} - read out loud {famous_person.title()}.
+{Fore.YELLOW}
+"That's strange..."{Fore.WHITE} - {famous_person.title()} added.
+{Fore.BLUE}
+"Why the text and the rose on the coin are {color_adj2}?"
+{Fore.WHITE} - pondered the {adj1} {noun1}.
+{Fore.YELLOW}
+"Maybe because the {size_adj2} {noun6} is {color_adj2}?"
+{Fore.WHITE}
+- {famous_person.title()} replied to {formatting_personal_pronoun(noun1)}.
 
-    {Fore.BLUE}"What is written there?"{Fore.WHITE} - asked the {noun1}.
+As soon as they got closer to the {size_adj2} {color_adj2} {noun6},
+the {noun1} yelled and started {verb_infinitive(verb1)}:
+{Fore.BLUE}
+"That's it!"
+{Fore.WHITE}
+On the front side of the {size_adj2} {color_adj2} {noun6} were
+a picture of a small version of {choose_article(size_adj2, color_adj2, noun6)}
+and a plate with 4 holes with sentences near them.
 
-    {Fore.YELLOW}"{idioms_list[0]}"{Fore.WHITE} - read out loud {famous_person.title()}.
+{draw_holes(idioms_list[1][0])}
+{draw_holes(idioms_list[1][1])}
+{draw_holes(idioms_list[1][2])}
+{draw_holes(idioms_list[1][3])}
+{Fore.YELLOW}
+"Listen, {noun1}! As I've got it,
+we just need to {verb4} and throw our coin into the right hole."
+{Fore.WHITE} - said {famous_person.title()}.
 
-    {Fore.YELLOW}"That's strange..."{Fore.WHITE} - {famous_person.title()} added.
+{Fore.BLUE}
+"Why do we need to {verb4} before throwing this coin?"
+{Fore.WHITE} - asked the {adj1} {noun1}.
+{Fore.YELLOW}
+"That's obvious!
+Because the old {noun7} was {verb_infinitive(verb4)}!"
+{Fore.BLUE}
+"Ok! Now you have to figure out which whole is needed."
+{Fore.YELLOW}
+"Why me? I am not so savvy!"
+{Fore.BLUE}
+"Look! I am just a poor {noun1}!
+I have only {plural_noun(noun5)}! No brain!
+So it is you, {famous_person.title()}, who have to make a decision!"
+{Fore.YELLOW}
+"Why me?"
+{Fore.BLUE}
+"It's deffinitely a strange question!
+The answer is simple: because you have a lot of {plural_noun(noun4)}
+and you are famous!"
+{Fore.YELLOW}
+"That's fair... But still my
+{plural_noun(noun4)} {choose_correct_form_be(noun4)} not helpful here.
+Let me think...
+If I were an old {noun7}, what would I choose..."
+{Fore.WHITE}
+After pondering for an hour,
+{famous_person.title()} took a coin and through it
+into the hole with the sentence:
+{Fore.RED}
+"{idioms_dictionary.get(idioms_list[0])}"{Fore.WHITE}.
+As soon as the coin disappeared in the hole, the wind blew and
+the ground opened up under them and swallowed them literally.
+They were falling down to nowhere...
+no light, no even a soul, just darkness everywhere...
+Eventually, the reached the ground.
+As luck would have it, they survived!
+{Fore.BLUE}
+"Why all of this is happenning to us?"
+{Fore.WHITE} - yelled the already terrified {noun1}.
+{Fore.YELLOW}
+"I sincerely can't get my head around this as well!
+It might be all about...
+{choose_article(size_adj1, color_adj1, noun3)}..."
+{Fore.WHITE} - replied {famous_person.title()}.
+{Fore.BLUE}
+"Yeah!
+I heard the the quality of the {size_adj1} {color_adj1} {noun3}
+is just marvelous!"{Fore.WHITE}.
 
-    {Fore.BLUE}"Why the text and the rose on the coin are {color_adjective2}?"
-    {Fore.WHITE} - pondered the {adj1} {noun1}.
+Had it not been for the lack of light,
+they would have realized on time that they were not alone.
+Then the light turned on and they saw everything...
+They were in a huge room, where everything was colored in {color_adj2}.
+They were surrounded by millions of old {plural_noun(noun7)}
+with their spears pointing at our travellers.
 
-    {Fore.YELLOW}"Maybe because the {size_adjective2} {noun6} is {color_adjective2}?"
-    {Fore.WHITE} - {famous_person.title()} replied to {formatting_personal_pronoun(noun1)}.
+{Fore.BLUE}"What's going on?"{Fore.WHITE} - whispered the poor {noun1}.
 
-    As soon as they got closer to the {size_adjective2} {color_adjective2} {noun6},
-    the {noun1} yelled and started {verb_infinitive(verb1)}:
+{Fore.YELLOW}"Unacceptable!
+I am {famous_person.title()}!
+You have to know who I am! Let us go!"
 
-    {Fore.BLUE}"That's it!"
+{Fore.RED}"Silence!"{Fore.WHITE} - one of the old {plural_noun(noun7)} ordered.
+{Fore.RED}"You think you are allowed to be cocky!?"
 
-    {Fore.WHITE}On the front side of the {size_adjective2} {color_adjective2} {noun6} were
-    a picture of a small version of {choosing_article(size_adjective2, color_adjective2, noun6)}
-    and a plate with 4 holes with sentences near them.
+{Fore.YELLOW}"Ok, let's make a deal!
+You see, I am in possession of lots of {plural_noun(noun4)}!
+So I will give you half of my fortune if you simply let us go
+with {choose_article(size_adj1, color_adj1, noun3)}"
 
-    {draw_holes(idioms_list[1][0])}
-    {draw_holes(idioms_list[1][1])}
-    {draw_holes(idioms_list[1][2])}
-    {draw_holes(idioms_list[1][3])}
+{Fore.RED}"Pathetic!"{Fore.WHITE} - the same old {plural_noun(noun7)} responded
+- {Fore.RED}Your {plural_noun(noun4)} {choose_correct_form_be(noun4)} nothing!
+But you two stole the golden coin!"
 
-    {Fore.YELLOW}"As I've got it, we just need to {verb4} and throw our coin into the right hole."
-    {Fore.WHITE} - said {famous_person.title()}.
+{Fore.BLUE}"Excuse me, sir"{Fore.WHITE} - mumbled {noun1} -
+{Fore.BLUE}"That's not true!
+One of the you gave it to us,
+{conjugate(verb4, tense = PAST)}, and left silently".
 
-    {Fore.BLUE}"Why do we need to {verb4} before throwing this coin?"
-    {Fore.WHITE} - asked the {adj1} {noun1}.
-
-    {Fore.YELLOW}"That's obvious!
-    Because the old {noun7} was {verb_infinitive(verb4)}!"
-
-    {Fore.BLUE}"Ok! Now you have to figure out which whole is needed."
-
-    {Fore.YELLOW}"Why me? I am not so savvy!"
-
-    {Fore.BLUE}"Look! I am just a poor {noun1}!
-    I have only {plural_noun(noun5)}! No brain!
-    So it is you, {famous_person.title()}, who have to make a decision!"
-
-    {Fore.YELLOW}"Why me?"
-
-    {Fore.BLUE}"It's deffinitely a strange question!
-    The answer is simple -  because you have a lot of {plural_noun(noun4)} and you are famous!"
-
-    {Fore.YELLOW}"That's fair...
-    But still my {plural_noun(noun4)} {choose_correct_form_be(noun4)} not helpful here.
-    Let me think...
-    If I were an old {noun7}, what would I choose..."
-
-    {Fore.WHITE}After pondering for an hour, {famous_person.title()} took a coin and through it
-    into the hole with the sentence {Fore.RED}"{idioms_dictionary.get(idioms_list[0])}"{Fore.WHITE}.
-    As soon as the coin disappeared in the hole, the wind blew and
-    the ground opened up under them and swallowed them literally.
-    They were falling down to nowhere...
-    no light, no even a soul, just darkness everywhere...
-    Eventually, the reached the ground.
-    As luck would have it, they survived!
-
-    {Fore.BLUE}"Why all of this is happenning to us?"{Fore.WHITE} - yelled the already terrified {noun1}.
-
-    {Fore.YELLOW}"I sincerely can't get my head around this as well!
-    It might be all about {choosing_article(size_adjective1, color_adjective1, noun3)}..."
-    {Fore.WHITE} - replied {famous_person.title()}.
-
-    {Fore.BLUE}"Yeah!
-    I heard the the quality of the {size_adjective1} {color_adjective1} {noun3} is marvelous!"{Fore.WHITE}.
-
-    Had it not been for the lack of light, they would have realized on time that they were not alone.
-    Then the light turned on and they saw everything...
-    They were in a huge room, where everything was colored in {color_adjective2}.
-    They were surrounded by millions of old {plural_noun(noun7)} with their spears pointing at our travellers.
-
-    {Fore.BLUE}"What's going on?"{Fore.WHITE} - whispered the poor {noun1}.
-
-    {Fore.YELLOW}"Unacceptable!
-    I am {famous_person.title()}!
-    You have to know who I am! Let us go!"
-
-    {Fore.RED}"Silence!"{Fore.WHITE} - one of the old {plural_noun(noun7)} ordered.
-    {Fore.RED}"You think you are allowed to be cocky!?"
-
-    {Fore.YELLOW}"Ok, let's make a deal!
-    You see, I am in possession of lots of {plural_noun(noun4)}!
-    So I will give you half of my fortune if you simply let us go
-    with {choosing_article(size_adjective1, color_adjective1, noun3)}"
-
-    {Fore.RED}"Pathetic!"{Fore.WHITE} - the same old {plural_noun(noun7)} responded
-    - {Fore.RED}Your {plural_noun(noun4)} {choose_correct_form_be(noun4)} nothing!
-    But you two stole the golden coin!"
-
-    {Fore.BLUE}"Excuse me, sir"{Fore.WHITE} - mumbled {noun1} - {Fore.BLUE}"That's not true!
-    One of the you gave it to us, {conjugate(verb4, tense = PAST)}, and left silently".
-
-    {Fore.RED}"listen! You don't have the right to take anything from our folks!
-    But you did! Moreover, you also performed our thing!"
-    
-    {Fore.BLUE}"What thing? We did nothing!
-    We just wanted to have {choosing_article(size_adjective1, color_adjective1, noun3)} together!"
-
-    {Fore.RED}"You {conjugate(verb4, tense = PAST)} on perpose!"{Fore.WHITE}
-    - the old {noun7} turned to the old {plural_noun(noun7)} who were holding spears.
-    {Fore.RED}"Put them in prison! Now!".
-
-    {Fore.WHITE}The {plural_noun(noun7)} with spears stepped forward and grabbed our travelers.
-    They were dragging them through endless {color_adjective2} tunnel.
+{Fore.RED}"listen! You don't have the right to take anything from our folks!
+But you did! Moreover, you also performed our thing!"
+{Fore.BLUE}
+"What thing? We did nothing! We just wanted to have
+{choose_article(size_adj1, color_adj1, noun3)} together!"
+{Fore.RED}
+"You {conjugate(verb4, tense = PAST)} on perpose!"
+{Fore.WHITE} - the old {noun7}
+turned to the old {plural_noun(noun7)} who were holding spears.
+{Fore.RED}"Put them in prison! Now!".
+{Fore.WHITE}
+The {plural_noun(noun7)} with spears stepped forward and grabbed our travelers.
+They were dragging them through endless {color_adj2} tunnel.
 
     '''
     print(madlib)
