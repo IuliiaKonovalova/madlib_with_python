014a256a2d1e01cd1b1b9e0d1d2694681102566e
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-09 16:34:39 +0800

    Add function to convert noun into reflexive pronoun

--- run.py
+++ run.py
@@ -71,6 +71,23 @@ def formatting_personal_pronoun(noun):
     return pronoun
 
 
+def formatting_reflexive_pronoun(noun):
+    """
+    Checks the noun whether it is a female or male noun,
+    places the correct personal pronoun when it is needed
+    """
+    # Checks if the word classified as female, replace the word with 'her'
+    if noun in dictionary_gender['female']:
+        pronoun = 'herself'
+    # Checks if the word classified as male, replace the word with 'him'
+    elif noun in dictionary_gender['male']:
+        pronoun = 'himself'
+    # Otherwise, returns 'it'
+    else:
+        pronoun = 'itself'
+    return pronoun
+
+
 def verb_infinitive(verb):
     """
     Format verb into infinitive
@@ -235,137 +252,6 @@ def draw_holes(mean):
 {sth1}{Fore.RESET}'''
 
 
-def main_madlib_2():
-    """
-    Asks for user input, checks the input,
-    prints the madlib strings with time interval
-    """
-    # Variables for a user's input
-    adj1 = ''
-    noun1 = ''
-    verb1 = ''
-    adj2 = ''
-    noun2 = ''
-    verb2 = ''
-    noun3 = ''
-    adj3 = ''
-    noun4 = ''
-
-    while len(adj1) < 2:
-        adj1 = input('Type an adjective: ').strip()
-    while len(noun1) < 2:
-        noun1 = input('Type a noun (living thing): ').strip()
-    while len(verb1) < 2:
-        verb1 = input('Type a verb: ').strip()
-    while len(adj2) < 2:
-        adj2 = input('Type an adjective: ').strip()
-    while len(noun2) < 2:
-        noun2 = input('Type a noun: ').strip()
-    while len(verb2) < 2:
-        verb2 = input('Type a verb: ').strip()
-    while len(noun3) < 2:
-        noun3 = input('Type a noun: ').strip()
-    while len(adj3) < 2:
-        adj3 = input('Type a adjective: ').strip()
-    while len(noun4) < 2:
-        noun4 = input('Type a noun: ').strip()
-    madlib_1 = f'''
-{Style.BRIGHT}{Fore.WHITE}
-A long time ago...
-In a galaxy named {Fore.YELLOW}{noun2.capitalize()}{Fore.WHITE}...
-{Fore.BLUE}{plural_noun(noun1).capitalize()}{Fore.WHITE} were living in peace.
-These {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE} loved
-{verb_infinitive(verb1)} very much.
-They {conjugate(verb1, tense = PAST)} without stopping because
-of their belief that {Fore.YELLOW}the {noun2.capitalize()} galaxy{Fore.WHITE}
-was thriving on their favorite activity: {verb_infinitive(verb1)}.
-
-There were thousands of planets in this galaxy and almost all of them were
-inhabited with {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE}.
-Some were {adj1} and some were {adj2}.
-
-The main purpose of their lives was to earn
-{Fore.YELLOW}the {noun2.capitalize()} status{Fore.WHITE}.
-This status meant everything to the {Fore.BLUE}{plural_noun(noun1)}
-{Fore.WHITE}, no matter whether they were {adj1} or {adj2}.
-{Fore.BLUE}{choose_article(noun2).capitalize()}{Fore.WHITE}, which reached
-{Fore.YELLOW}the {noun2.capitalize()} status{Fore.WHITE} were called
-{Fore.YELLOW}{noun2.capitalize()}-master{Fore.WHITE}.
-
-{Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} were empowered to learn
-about other galaxies and even explore them.
-To obtain this status, {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE} had to
-exchange goods with each other without any quarrels and {verb1} as much
-as possible during their lives.
-
-The most fascinating thing about all this {Fore.YELLOW}{noun2.capitalize()}
-galaxy{Fore.WHITE} was that they were using {plural_noun(noun3)}
-to communicate with each other inside their galaxy.
-Of course, only {Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE}
-were aware of how to built those {plural_noun(noun3)}.
-This meant that common {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE} depended on
-masters a lot. However, masters needed to get materials to built
-{plural_noun(noun3)}. And those materials were in the Galaxy called
-{Fore.RED}{noun3.capitalize()}{Fore.WHITE}.
-
-This galaxy was a very dangerous one because it had
-a {Fore.RED}Ruler{Fore.WHITE}, who hated {Fore.YELLOW}
-the {noun2.capitalize()} galaxy{Fore.WHITE} and did what ever it takes to
-destroy visitors from that galaxy.
-
-As {Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} were using {adj3}
-{plural_noun(noun4)} to travel beyond {Fore.YELLOW}the {noun2.capitalize()}
-galaxy{Fore.WHITE}, they {conjugate(verb2, tense = PAST)} with lots of power
-as cosmos was a hostile environment for their {adj3} {plural_noun(noun4)}.
-
-So the {Fore.RED}Ruler{Fore.WHITE} of {Fore.RED}the {noun3.capitalize()} galaxy
-{Fore.WHITE} was always waiting for the
-{adj3} {plural_noun(noun4)} to enter his territories.
-As soon as he was informed that {choose_article(adj3, noun4)}
-enters his galaxy, he ordered all of the living things in {Fore.RED}
-the {noun3} galaxy{Fore.WHITE} to {verb1}.
-It created the illusion for {Fore.YELLOW}{noun2.capitalize()}-
-masters{Fore.WHITE} that they were entering their
-own galaxy and they were stopping {verb_infinitive(verb2)} and as a
-consequence - losing control of their {plural_noun(noun4)}.
-
-Fortunately for the communication in the galaxy where {Fore.BLUE}{adj1} and
-{adj2} {plural_noun(noun1)}{Fore.WHITE} were living,
-it was not always the case, and thus, they were getting their
-{plural_noun(noun3)} to chat with each other.
-
-But once one of the {Fore.BLUE}{adj1} {plural_noun(noun1)}{Fore.WHITE} decided
-to talk {Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} into taking over
-{Fore.RED}the {noun3.capitalize()} galaxy{Fore.WHITE} to make the recourses
-for building their communication devices easier to obtain.
-{Fore.BLUE}
-"We have to make it easier for our folks to perform {verb_infinitive(verb1)}!
-It is the most enjoyable activity for us!
-We can't allow some greedy {Fore.RED}Ruler{Fore.WHITE} to take it from us!
-Let's make more {adj3} {plural_noun(noun4)} and put more {noun1} in them that
-we could travel beyond our galaxy all together!"
-{Fore.YELLOW}
-"We cannot let simple {Fore.BLUE}{noun1}{Fore.YELLOW} to go farther than
-our terretory unprepared!
-They have to learn their way to the appropriate status!
-It is the low of our galaxy!"
-{Fore.BLUE}
-I do know it!
-But desperate times call for desperate measures!
-We have to make an exception for the sake of our galaxy and {noun1}
-living here!"
-
-
-
-
-
-
-
-
-    '''
-    print(madlib_1)
-
-
 def main_madlib():
     """
     Asks for user input, checks the input, return the madlib formatted string
@@ -441,8 +327,8 @@ decided to {verb3} and have
 {choose_article(size_adj1, color_adj1, noun3)} together.
     ''',
         f'''
-{Style.BRIGHT}
-{Fore.BLUE}"It will cost an arm and a leg!"{Fore.WHITE} - said the {noun1}.
+{Style.BRIGHT}{Fore.BLUE}"It will cost an arm and a leg!"
+{Fore.WHITE}- said the {noun1}.
 {Fore.YELLOW}
 "Do not worry! I have a lot of {plural_noun(noun4)}! I can afford it!"
 {Fore.BLUE}
@@ -518,15 +404,13 @@ and a plate with 4 holes with sentences near them.
 {draw_holes(idioms_list[1][3])}
     ''',
         f'''
-{Style.BRIGHT}
-{Fore.YELLOW}"Listen, {noun1}! As I've got it,
+{Style.BRIGHT}{Fore.YELLOW}"Listen, {noun1}! As I've got it,
 we just need to {verb4} and throw our coin into the right hole."
 {Fore.WHITE}- said {famous_person.title()}.
     ''',
         f'''
-{Style.BRIGHT}
-{Fore.BLUE}"Why do we need to {verb4} before throwing this coin?"
-{Fore.WHITE}- asked the {adj1} {noun1}.
+{Style.BRIGHT}{Fore.BLUE}"Why do we need to {verb4} before
+throwing this coin?"{Fore.WHITE}- asked the {adj1} {noun1}.
 {Fore.YELLOW}
 "That's obvious!
 Because the old {noun7} was {verb_infinitive(verb4)}!"
@@ -788,6 +672,180 @@ def show_the_rules():
     sleep(1)
 
 
+def main_madlib_2():
+    """
+    Asks for user input, checks the input,
+    prints the madlib strings with time interval
+    """
+    # Variables for a user's input
+    adj1 = ''
+    noun1 = ''
+    verb1 = ''
+    adj2 = ''
+    noun2 = ''
+    verb2 = ''
+    noun3 = ''
+    adj3 = ''
+    noun4 = ''
+
+    while len(adj1) < 2:
+        adj1 = input('Type an adjective: ').strip()
+    while len(noun1) < 2:
+        noun1 = input('Type a noun (living thing): ').strip()
+    while len(verb1) < 2:
+        verb1 = input('Type a verb: ').strip()
+    while len(adj2) < 2:
+        adj2 = input('Type an adjective: ').strip()
+    while len(noun2) < 2:
+        noun2 = input('Type a noun: ').strip()
+    while len(verb2) < 2:
+        verb2 = input('Type a verb: ').strip()
+    while len(noun3) < 2:
+        noun3 = input('Type a noun: ').strip()
+    while len(adj3) < 2:
+        adj3 = input('Type a adjective: ').strip()
+    while len(noun4) < 2:
+        noun4 = input('Type a noun: ').strip()
+    madlib_2 = f'''
+{Style.BRIGHT}{Fore.WHITE}
+A long time ago...
+In a galaxy named {Fore.YELLOW}{noun2.capitalize()}{Fore.WHITE}...
+{Fore.BLUE}{plural_noun(noun1).capitalize()}{Fore.WHITE} were living in peace.
+These {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE} loved
+{verb_infinitive(verb1)} very much.
+They {conjugate(verb1, tense = PAST)} without stopping because
+of their belief that {Fore.YELLOW}the {noun2.capitalize()} galaxy{Fore.WHITE}
+was thriving on their favorite activity: {verb_infinitive(verb1)}.
+
+There were thousands of planets in this galaxy and almost all of them were
+inhabited with {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE}.
+Some were {adj1} and some were {adj2}.
+
+The main purpose of their lives was to earn
+{Fore.YELLOW}the {noun2.capitalize()} status{Fore.WHITE}.
+This status meant everything to the {Fore.BLUE}{plural_noun(noun1)}
+{Fore.WHITE}, no matter whether they were {adj1} or {adj2}.
+{Fore.BLUE}{choose_article(noun2).capitalize()}{Fore.WHITE}, which reached
+{Fore.YELLOW}the {noun2.capitalize()} status{Fore.WHITE} were called
+{Fore.YELLOW}{noun2.capitalize()}-master{Fore.WHITE}.
+
+{Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} were empowered to learn
+about other galaxies and even explore them.
+To obtain this status, {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE} had to
+exchange goods with each other without any quarrels and {verb1} as much
+as possible during their lives.
+
+The most fascinating thing about all this {Fore.YELLOW}{noun2.capitalize()}
+galaxy{Fore.WHITE} was that they were using {plural_noun(noun3)}
+to communicate with each other inside their galaxy.
+Of course, only {Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE}
+were aware of how to built those {plural_noun(noun3)}.
+This meant that common {Fore.BLUE}{plural_noun(noun1)}{Fore.WHITE} depended on
+masters a lot. However, masters needed to get materials to built
+{plural_noun(noun3)}. And those materials were in the Galaxy called
+{Fore.RED}{noun3.capitalize()}{Fore.WHITE}.
+
+This galaxy was a very dangerous one because it had
+a {Fore.RED}Ruler{Fore.WHITE}, who hated {Fore.YELLOW}
+the {noun2.capitalize()} galaxy{Fore.WHITE} and did what ever it takes to
+destroy visitors from that galaxy.
+
+As {Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} were using {adj3}
+{plural_noun(noun4)} to travel beyond {Fore.YELLOW}the {noun2.capitalize()}
+galaxy{Fore.WHITE}, they {conjugate(verb2, tense = PAST)} with lots of power
+as cosmos was a hostile environment for their {adj3} {plural_noun(noun4)}.
+
+So the {Fore.RED}Ruler{Fore.WHITE} of {Fore.RED}the {noun3.capitalize()} galaxy
+{Fore.WHITE} was always waiting for the
+{adj3} {plural_noun(noun4)} to enter his territories.
+As soon as he was informed that {choose_article(adj3, noun4)}
+enters his galaxy, he ordered all of the living things in {Fore.RED}
+the {noun3} galaxy{Fore.WHITE} to {verb1}.
+It created the illusion for {Fore.YELLOW}{noun2.capitalize()}-
+masters{Fore.WHITE} that they were entering their
+own galaxy and they were stopping {verb_infinitive(verb2)} and as a
+consequence - losing control of their {plural_noun(noun4)}.
+
+Fortunately for the communication in the galaxy where {Fore.BLUE}{adj1} and
+{adj2} {plural_noun(noun1)}{Fore.WHITE} were living,
+it was not always the case, and thus, they were getting their
+{plural_noun(noun3)} to chat with each other.
+
+But once one of the {Fore.BLUE}{adj1} {plural_noun(noun1)}{Fore.WHITE} decided
+to talk {Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} into taking over
+{Fore.RED}the {noun3.capitalize()} galaxy{Fore.WHITE} to make the recourses
+for building their communication devices easier to obtain.
+{Fore.BLUE}
+"We have to make it easier for our folks to perform {verb_infinitive(verb1)}!
+It is the most enjoyable activity for us!
+We can't allow some greedy {Fore.RED}Ruler{Fore.WHITE} to take it from us!
+Let's make more {adj3} {plural_noun(noun4)} and put more {noun1} in them that
+we could travel beyond our galaxy all together!"
+{Fore.YELLOW}
+"We cannot let simple {Fore.BLUE}{noun1}{Fore.YELLOW} to go farther than
+our terretory unprepared!
+They have to learn their way to the appropriate status!
+It is the low of our galaxy!"
+{Fore.BLUE}
+"I do know it!
+But desperate times call for desperate measures!
+We have to make an exception for the sake of our galaxy
+and {plural_noun(noun1)} living here!"
+{Fore.YELLOW}
+"We have to take time to ponder it.
+Let us think it through first cause it is all about the survival of our galaxy,
+and {plural_noun(noun1)} as well."
+{Fore.BLUE}
+"That's what I'm talking about! Take your time, and call me after you come up
+with your decision!"
+{Fore.WHITE}
+As {Fore.BLUE}{choose_article(adj1, noun1)}{Fore.WHITE} left the meeting,
+{Fore.YELLOW}{noun2.capitalize()}-masters{Fore.WHITE} started their discussion.
+There were plenty of arguments for and against taking over
+{Fore.RED}the {noun3.capitalize()} galaxy{Fore.WHITE}. Some were insisting
+that such an adventure may bring tremendous loses and tons of troubles rather
+than unlimited resources of materials for {plural_noun(noun3)}. Others were
+having conserns that it may also be quite costly to create so many
+{adj3} {plural_noun(noun4)} for just a singe voyage. However, there were some
+supporters of {Fore.BLUE}{choose_article(adj1, noun1)}{Fore.WHITE}'s idea,
+and their main argument was that all these issues with obtaining materials
+for {plural_noun(noun3)} were pretty exasperating since traveling to the same
+hostile galaxy every now and then to get the materials was driving every one
+up the wall. All this never ending {verb_infinitive(verb2)} while moving
+on their {plural_noun(noun4)}. And even the fact that {Fore.RED}the Ruler
+{Fore.WHITE} was misleading {Fore.YELLOW}{noun2.capitalize()}-masters
+{Fore.WHITE} by using their own ritual of {verb_infinitive(verb1)}, which
+had been passed from generation to generation in {Fore.YELLOW}
+the {noun2.capitalize()} galaxy{Fore.WHITE}, was significantly unbearable for
+everyone.
+
+After all these debates, the {Fore.BLUE}{adj1} {noun1}{Fore.WHITE}'s
+supporters {conjugate(verb1, tense = PAST)} and convinced the rest of
+the masters to follow them. Although, the opposition had agreed, they presented
+the supporters with an ultimatum. According to this ultimatum,
+{Fore.BLUE}{choose_article(adj1, noun1)}{Fore.WHITE} should pass a special
+text first to prove that {format_pronoun(noun1)} is worthy.
+
+The {Fore.BLUE}{adj1} {noun1}{Fore.WHITE} was called to the masters to attend
+the event that had been created to check if {format_pronoun(noun1)} was worthy.
+He was given a list with some text and left alone in a small version of
+{choose_article(adj2, noun4)}.
+{Fore.BLUE}
+"That's interesting and at the same time pretty engaging..."{Fore.WHITE}
+- said the {Fore.BLUE}{adj1} {noun1}{Fore.WHITE} to
+{formatting_reflexive_pronoun(noun1)} and started reading.
+
+It was some kind of a quiz, which consisted to lines filled with words and
+one empty line at the end:
+
+
+
+
+    '''
+    print(madlib_2)
+
+
+
 def main():
     """
     Main program function
@@ -811,7 +869,7 @@ Please, pick an option in the {Fore.YELLOW}menu!
     sleep(1)
     options = ['1. Learn the rules', '2. Start the game', '3. Quit']
     main_menu = TerminalMenu(options)
-    sub_options = ['1. Story 1', '2. Story 2', '3. Story 3', '4. Go back']
+    sub_options = ['1. Story 1', '2. Story 2', '3. Go back']
     sub_menu = TerminalMenu(sub_options)
     quitting = False
     while quitting is not True:
@@ -854,11 +912,6 @@ Please read the rules attentively and follow them precisely!
                 print(message_to_user_1)
                 sleep(2)
                 main_madlib_2()
-                print(message_to_user_2)
-            elif suboptions_choice == '2. Story 3':
-                print(message_to_user_1)
-                sleep(2)
-                main_madlib_3()
                 print(message_to_user_2)
             else:
                 options_index = main_menu.show()
